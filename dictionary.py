508bf768391194cac6ae78a65f864657f3b74948
kashif699 <kashif4737@gmail.com>
2024-07-01 15:36:12 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,30 @@
+#an unordered, mutable  collection of key-value pairs.
+
+#creating a dictionary
+my_dict = { 'name' : 'kashif', 'age' : 20, 'city' : 'new_delhi' }
+
+print(my_dict) #{'name': 'kashif', 'age': 20, 'city': 'new_delhi'}
+
+
+# Accessing values
+print(my_dict['city']) #new_delhi
+
+
+#updating a existing value
+my_dict['age'] = 21
+print(my_dict['age']) #21
+
+
+#adding a new key-value pair
+my_dict['email'] = 'kashif4737@gmail.com'
+
+print(my_dict) #{'name': 'kashif', 'age': 20, 'city': 'new_delhi', 'email': 'kashif4737@gmail.com'}
+
+
+#removing  a key-value pair
+del my_dict['email']
+
+print(my_dict) #{'name': 'kashif', 'age': 21, 'city': 'new_delhi'}
+
+
+      
